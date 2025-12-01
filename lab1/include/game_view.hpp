a22fb7f8f9f0093ffe2bb5956ad931705f52b002
chilya02 <ilya.cherepanov.02@mail.ru>
2025-12-01 22:22:22 +0300

    defines refactored

--- lab1/include/game_view.hpp
+++ lab1/include/game_view.hpp
@@ -8,6 +8,22 @@
 #include <ncurses.h>
 #include <vector>
 
+#define DEFAULT_COLOR       0
+#define CELL_COLOR          1
+#define IMPASSABLE_COLOR    2
+#define SLOW_COLOR          3
+#define PLAYER_COLOR        4
+#define AWAIT_COLOR         5
+#define SLOWED_COLOR        6
+
+#define PLAYER_SYM          "/\\"
+#define SLOW_SYM            "~~"
+#define IMPASSABLE_SYM      "MM"
+#define CELL_SYM            "  "
+
+#define CELL_WIDTH          2
+#define CELL_HEIGHT         1
+
 class GameView{
   public:
     GameView(GameField* field, Player* player);
--- lab1/src/game_view.cpp
+++ lab1/src/game_view.cpp
@@ -4,22 +4,6 @@
 
 #include <vector>
 
-#define DEFAULT_COLOR       0
-#define CELL_COLOR          1
-#define IMPASSABLE_COLOR    2
-#define SLOW_COLOR          3
-#define PLAYER_COLOR        4
-#define AWAIT_COLOR         5
-#define SLOWED_COLOR        6
-
-#define PLAYER_SYM          "/\\"
-#define SLOW_SYM            "~~"
-#define IMPASSABLE_SYM      "MM"
-#define CELL_SYM            "  "
-
-#define CELL_WIDTH          2
-#define CELL_HEIGHT         1
-
 GameView::GameView(GameField* field, Player* player):field(field), player(player){
   this->field_scr_width = field->width * CELL_WIDTH * 2;
   this->field_scr_height = field->height * CELL_HEIGHT * 2;
