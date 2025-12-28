1de47e6a75a5157b6a4479e2f42f5351d9b1cbe7
chilya02 <ilya.cherepanov.02@mail.ru>
2025-12-28 13:20:22 +0300

    feat(spells): add card interfaces

---
+++ lab1/include/spells/movable_spell_card_interface.hpp
@@ -0,0 +1,16 @@
+#ifndef MOVABLE_SPELL_CARD_H
+#define MOVABLE_SPELL_CARD_H
+
+#include "spell_card_interface.hpp"
+#include "../controllers/movable_spell_controller.hpp"
+#include "movable_spell.hpp"
+
+class MovableSpellCardInterface:public SpellCardInterface{
+  public:
+    MovableSpellController* get_controller() override = 0;
+  protected:
+    MovableSpell* spell;
+    MovableSpellController* conrtoller;
+};
+
+#endif //MOVABLE_SPELL_CARD_H
