15cdd21e5f5538849fc217a062202054b83934e9
chilya02 <ilya.cherepanov.02@mail.ru>
2025-12-28 13:50:05 +0300

    refactor (spells): rename from spell to damage

--- lab1/include/spells/movable_spell_card_interface.hpp
+++ lab1/include/spells/movable_spell_card_interface.hpp
@@ -3,13 +3,13 @@
 
 #include "spell_card_interface.hpp"
 #include "../controllers/movable_spell_controller.hpp"
-#include "movable_spell.hpp"
+#include "movable_damage.hpp"
 
 class MovableSpellCardInterface:public SpellCardInterface{
   public:
     MovableSpellController* get_controller() override = 0;
   protected:
-    MovableSpell* spell;
+    MovableDamage* spell;
     MovableSpellController* conrtoller;
 };
 
